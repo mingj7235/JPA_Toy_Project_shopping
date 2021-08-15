dcc5e34ac547e798169f64913bec90bb015e5c39
김민재 <joshuara7235@gmail.com>
2021-08-15 11:51:07 +0900

    Importance of using dto for API making

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +17,26 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    @PostMapping ("/api/vi/members")
+    @PostMapping ("/api/v1/members")
     public CreateMemberResponse saveMemberV1 (@RequestBody @Valid Member member) {
         //@RequestBody : json으로 온것을 Member로 담아준다.
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PostMapping ("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    } //실무에서는 절대로 API에서 entity를 받고 내보내지 않는다. DTO로 작업한다.
+
+    @Data
+    static class CreateMemberRequest { //DTO
+        @NotEmpty
+        private String name;
     }
 
     @Data
