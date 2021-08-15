2e621b359af2cc0139ea7f06572232337a57cac7
김민재 <joshuara7235@gmail.com>
2021-08-15 21:54:11 +0900

    Explanation for v1 api

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -17,6 +17,17 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    /**
+     * 등록 v1 : 요청 값으로 Member 엔티티를 직접 받는다.
+     * 문제점
+     * 1. 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * 2. 엔티티에 API검증을 위한 로직이 들어간다. (@NotEmpty등)
+     * 3. 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 모든 요청 요구사항을 담기는 어렵다.
+     * 4. 엔티티가 변경되면 API 스펙이 변한다.
+     *
+     * 결론
+     * - API 요청 스펙에 맟추어 별도의 DTO를 파라미터로 받는다.
+     */
     @PostMapping ("/api/v1/members")
     public CreateMemberResponse saveMemberV1 (@RequestBody @Valid Member member) {
         //@RequestBody : json으로 온것을 Member로 담아준다.
